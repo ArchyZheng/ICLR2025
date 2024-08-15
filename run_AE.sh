f0aceebdf5d5ea14aa5c63f3575f39235bd97a26
Archy <archyzhengchengqi@gmail.com>
2024-08-15 10:29:06 +0800

    refactor: Update CUDA_VISIBLE_DEVICES in run_AE.sh

--- run_AE.sh
+++ run_AE.sh
@@ -1,5 +1,6 @@
-export CUDA_VISIBLE_DEVICES="3"
+export CUDA_VISIBLE_DEVICES="0"
 export WANDB_API_KEY=d3be0188f8e6de441fe26438708884794c8db33f
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
+# export XLA_PYTHON_CLIENT_MEM_FRACTION=.95
 
 /home/haiyan/anaconda3/envs/dev_co/bin/python train_cotasp.py
